316aaec96337cf767f2790c3a4983fe0b9e0279e
PaingSC <paingsoechit.job@gmail.com>
2024-01-19 14:58:08 +0630

    Updating:Form: adding form details

--- src/App.js
+++ src/App.js
@@ -21,10 +21,24 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h2>What do you need for your 😍 trip?</h2>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((cur) => (
+          <option value={cur} key={cur}>
+            {cur}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
